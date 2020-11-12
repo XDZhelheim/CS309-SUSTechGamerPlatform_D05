13a933c619050b3fd0aebe78666ad91346e00c7c
XDZhelheim <601251915@qq.com>
2020-11-12 10:39:42 +0800

    front

--- appfront-vue/src/App.vue
+++ appfront-vue/src/App.vue
@@ -1,5 +1,25 @@
 <template>
     <div id="app">
+        <header>
+            <el-menu
+            :default-active="activeIndex"
+            class="el-menu-demo"
+            mode="horizontal"
+            @select="handleSelect"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+            router>
+                <el-menu-item index="/">首页</el-menu-item>
+                <el-menu-item index="/shop">游戏商城</el-menu-item>
+                <el-menu-item index="/my-games">我的游戏</el-menu-item>
+                <el-menu-item index="/community">游戏社区</el-menu-item>
+                <el-menu-item index="/customer-service">客服反馈</el-menu-item>
+                <el-menu-item index="/login" class="menu-right">登录</el-menu-item>
+                <el-menu-item index="/developer" class="menu-right">开发者</el-menu-item>
+                <el-menu-item index="/hello" class="menu-right">欢迎</el-menu-item>
+            </el-menu>
+        </header>
         <router-view />
     </div>
 </template>
@@ -11,7 +31,14 @@ export default {
 </script>
 
 <style scoped>
-    body {
-        background-color: #000;
+    header {
+        position: fixed;
+        top: 0%;
+        left: 0%;
+        width: 100%;
+    }
+
+    .el-menu-item.menu-right {
+        float: right;
     }
 </style>
--- appfront-vue/src/views/Home.vue
+++ appfront-vue/src/views/Home.vue
@@ -1,23 +1,5 @@
 <template>
     <div id="home">
-        <header>
-            <el-menu
-            :default-active="activeIndex"
-            class="el-menu-demo"
-            mode="horizontal"
-            @select="handleSelect"
-            background-color="#545c64"
-            text-color="#fff"
-            active-text-color="#ffd04b">
-            <el-menu-item index="1">游戏商城</el-menu-item>
-            <el-menu-item index="2">我的游戏</el-menu-item>
-            <el-menu-item index="3">游戏社区</el-menu-item>
-            <el-menu-item index="4">客服反馈</el-menu-item>
-            <el-menu-item index="6" class="menu-right">登录</el-menu-item>
-            <el-menu-item index="5" class="menu-right">开发者</el-menu-item>
-            </el-menu>
-        </header>
-
         <div id="placehold"></div>
 
         <div id="recent-and-poster">
@@ -52,7 +34,7 @@
         name: 'Home',
         data() {
             return {
-                activeIndex: '1',
+                activeIndex: '/shop',
                 bannerHeight: '',
                 recentgames: [
                     require('../assets/recentgames/test.jpg'),
@@ -90,17 +72,6 @@
 </script>
 
 <style scoped>
-    header {
-        position: fixed;
-        top: 0%;
-        left: 0%;
-        width: 100%;
-    }
-
-    .el-menu-item.menu-right {
-        float: right;
-    }
-
     #recent-and-poster {
         margin: 5px;
     }
@@ -114,13 +85,5 @@
     #r-game:nth-child(2), #r-game:nth-child(3) {
         margin-top: 7px;
     }
-
-    /* .el-carousel__container, .el-carousel--horizontal{
-        height: auto!important;
-    } */
-
-    /* .el-row, .el-col {
-        border: solid thin white;
-    } */
 
 </style>
